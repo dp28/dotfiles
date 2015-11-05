6256c5a0801701fd04e828952e246095a964b6f8
Daniel Patterson <dan24patt@gmail.com>
2015-11-05 20:59:17 +0000

    Don't track files created be the FileFactory
    Cleanup is handled by rsync now, so tracking is unnecessary.

--- spec/support/factories/file_factory.rb
+++ spec/support/factories/file_factory.rb
@@ -5,23 +5,12 @@ class FileFactory
     def create_file(path, content)
       ensure_dir_exists File.dirname path
       File.open(path, 'w') { |file| file.write content }
-      @@paths_created << path
-    end
-
-    def cleanup
-      @@paths_created ||= []
-      @@paths_created.each { |path| FileUtils.rm_rf(path) if File.exists? path }
-      @@paths_created = []
     end
 
     private
 
     def ensure_dir_exists(path)
-      @@paths_created ||= []
-      unless File.directory?(path)
-        @@paths_created << FileUtils.mkdir_p(path).first
-        @@paths_created << path.split('/').first
-      end
+      FileUtils.mkdir_p(path) unless File.directory? path
     end
   end
 end
