7642a89ed9bca246e955a3a879e07b414b66d8a1
Daniel Patterson <dan24patt@gmail.com>
2015-11-28 17:52:12 +0000

    Add zsh version of bash prompt

---
+++ src/zsh/prompt.zsh
@@ -0,0 +1,26 @@
+autoload -U promptinit && promptinit
+autoload -U colors && colors
+
+colour_string() {
+  echo "%{$fg_bold[$1]%}$2%{$reset_color%}"
+}
+
+in_git=$(git symbolic-ref --short HEAD 2>/dev/null)
+if [ -n "$in_git" ] ; then
+  repo=`colour_string blue "$(git_repo)"`
+  branch=`colour_string green "$(git_branch)"`
+  git_status=`colour_string yellow "$(git_branch_status)"`
+  PROMPT="$repo|$branch|$git_status"
+else
+  PROMPT="%n@%m"
+fi
+
+PROMPT="`date +%H:%M`|$PROMPT
+\$ "
+RPROMPT="%(?..[`colour_string red "%?"`])"
+
+unset in_git
+unset repo
+unset branch
+unset git_status
+unset -f colour_string
