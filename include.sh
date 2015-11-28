c07ee7110eb08e68930a0cf466eba0bd09559f62
Daniel Patterson <dan24patt@gmail.com>
2015-11-28 15:48:21 +0000

    Source shell-specific files only in that shell

--- include.sh
+++ include.sh
@@ -1,13 +1,21 @@
 #! /usr/bin/env bash
 
-for file in `find $DOTFILES_HOME/src -path '*/preload/*.sh'` ; do
-  . $file
-done
+source_all() {
+  local file
+  for file in "$@" ; do
+    . $file
+  done
+}
 
-for file in `find $DOTFILES_HOME/src -path '*.sh' ! -name 'include.sh' ! -path '*/preload/*.sh' ! -path '*/install/*'`
-do
-  . $file
-done
+source_all `find $DOTFILES_HOME/src -path '*/preload/*.sh'`
+
+source_all `find $DOTFILES_HOME/src -path '*.sh' ! -name 'include.sh' ! -path '*/preload/*.sh' ! -path '*/install/*'`
+
+if [ -n "$ZSH_VERSION" ]; then
+  source_all `find $DOTFILES_HOME/src -path '*.zsh'`
+elif [ -n "$BASH_VERSION" ]; then
+  source_all `find $DOTFILES_HOME/src -path '*.bash'`
+fi
 
 for bin_dir in `find $DOTFILES_HOME -path '*/bin'` ; do
   add_to_path $bin_dir
