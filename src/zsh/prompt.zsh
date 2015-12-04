6f8d40e2c4cedc5311cda0624d3d274b24d658ac
Daniel Patterson <dan24patt@gmail.com>
2015-12-04 17:32:47 +0000

    Evaluate the ZSH prompt before each command
    Not just on load ...
    
    Apparently 'precmd' is run before each command, similar(ish) to
    setting PROMPT_COMMAND in bash.

--- src/zsh/prompt.zsh
+++ src/zsh/prompt.zsh
@@ -5,22 +5,18 @@ colour_string() {
   echo "%{$fg_bold[$1]%}$2%{$reset_color%}"
 }
 
-in_git=$(git symbolic-ref --short HEAD 2>/dev/null)
-if [ -n "$in_git" ] ; then
-  repo=`colour_string blue "$(git_repo)"`
-  branch=`colour_string green "$(git_branch)"`
-  git_status=`colour_string yellow "$(git_branch_status)"`
-  PROMPT="$repo|$branch|$git_status"
-else
-  PROMPT="%n@%m"
-fi
+precmd() {
+  in_git=`git symbolic-ref --short HEAD 2>/dev/null`
+  if [ -n "$in_git" ] ; then
+    repo=`colour_string blue "$(git_repo)"`
+    branch=`colour_string green "$(git_branch)"`
+    git_status=`colour_string yellow "$(git_branch_status)"`
+    PROMPT="$repo|$branch|$git_status"
+  else
+    PROMPT="%n@%m"
+  fi
 
-PROMPT="`date +%H:%M`|$PROMPT
+  PROMPT="`date +%H:%M`|$PROMPT
 \$ "
-RPROMPT="%(?..[`colour_string red "%?"`])"
-
-unset in_git
-unset repo
-unset branch
-unset git_status
-unset -f colour_string
+  RPROMPT="%(?..[`colour_string red "%?"`])"
+}
