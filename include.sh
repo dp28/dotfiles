243de2ed01beaef60680c1bdad84ce39e4dbbf7b
Daniel Patterson <dan24patt@gmail.com>
2016-02-06 18:38:30 +0000

    Include all *sh files, not just .sh files

--- include.sh
+++ include.sh
@@ -1,13 +1,16 @@
 #! /usr/bin/env bash
 
-for file in `find $DOTFILES_HOME/src -path '*/preload/*.sh'` ; do
-  . $file
-done
+source_all() {
+  for file in $@ ; do
+    if [ -f $file ]; then
+      source $file
+    fi
+  done
+}
 
-for file in `find $DOTFILES_HOME/src -path '*.sh' ! -name 'include.sh' ! -path '*/preload/*.sh' ! -path '*/install/*'`
-do
-  . $file
-done
+source_all `find $DOTFILES_HOME/src -path '*/preload/*sh'`
+
+source_all `find $DOTFILES_HOME/src -path '*sh' ! -name 'include.sh' ! -path '*/preload/*sh' ! -path '*/install/*'`
 
 for bin_dir in `find $DOTFILES_HOME -path '*/bin'` ; do
   add_to_path $bin_dir
