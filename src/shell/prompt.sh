5035471eee4fa88172c19638ab686f8d6d7021f6
Daniel Patterson <djp@getadministrate.com>
2017-05-22 15:19:48 +0100

    Include directory in prompt if not top level repo

--- src/shell/prompt.sh
+++ src/shell/prompt.sh
@@ -6,6 +6,13 @@ build_prompt() {
     branch='\[\e[1;32m\]$(git_branch)\[\e[m\]'
     status='\[\e[1;33m\]$(git_branch_status)\[\e[00m\]'
     PS1="$repo|$branch|$status"
+
+    local repo_path=$(git rev-parse --show-toplevel)
+    local current_path=$(pwd)
+    if [ $repo_path != $current_path ] ; then
+      local repo_path="$repo_path/"
+      PS1="$repo/${current_path/$repo_path/}|$branch|$status"
+    fi
   else
     PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w '
   fi
