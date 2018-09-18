dp28/dotfiles

commit 7e8d9b461f5f501d9c090b8779adf03f1130643e

Don't assume branches track origin in grh

src/git/functions.sh
@@ -14,7 +14,7 @@ wip() {
 grh() {
   git fetch
   if [ $# -eq 0 ]; then
-    branch="origin/`gsw -c`"
+    branch=`git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD)
   else
     branch="$1"
   fi

commit 4f4a17290405a0f1586add5c302a95e6948855a1

Fix grh
Broken in 7e8d9b461f5f501d9c090b8779adf03f1130643e

src/git/functions.sh
@@ -14,7 +14,7 @@ wip() {
 grh() {
   git fetch
   if [ $# -eq 0 ]; then
-    branch=`git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD)
+    branch=`git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD)`
   else
     branch="$1"
   fi