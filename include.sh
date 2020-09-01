94dfbde8a3728567c473040a23a0e71e88d13835
Daniel Patterson <daniel.patterson@ezcater.com>
2020-09-01 14:04:46 -0400

    Source ".sh" files, not files ending in "sh"
    This caused the pre-push hook to be executed on whenever I reloaded my
    dotfiles, which unsurprisingly failed. That in turn prevented me opening
    new terminals ...

--- include.sh
+++ include.sh
@@ -8,9 +8,9 @@ source_all() {
   done
 }
 
-source_all `find $DOTFILES_HOME/src -path '*/preload/*sh'`
+source_all `find $DOTFILES_HOME/src -path '*/preload/*.sh'`
 
-source_all `find $DOTFILES_HOME/src -path '*sh' ! -name 'include.sh' ! -path '*/preload/*sh' ! -path '*/install/*' ! -path '*/.git/*'`
+source_all `find $DOTFILES_HOME/src -path '*.sh' ! -name 'include.sh' ! -path '*/preload/*.sh' ! -path '*/install/*' ! -path '*/.git/*'`
 
 for bin_dir in `find $DOTFILES_HOME -path '*/bin'` ; do
   add_to_path $bin_dir
