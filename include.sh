0d5cef542b08595b9c8d213cbb65f3b8d9bffc3b
Daniel Patterson <dan24patt@gmail.com>
2021-03-22 19:27:12 -0400

    Fix tms completion

--- include.sh
+++ include.sh
@@ -11,6 +11,7 @@ source_all() {
 source_all `find $DOTFILES_HOME/src -path '*/preload/*.sh'`
 
 source_all `find $DOTFILES_HOME/src -path '*.sh' ! -name 'include.sh' ! -path '*/preload/*.sh' ! -path '*/install/*' ! -path '*/.git/*'`
+source_all `find $DOTFILES_HOME/src -path '*.bash'`
 
 for bin_dir in `find $DOTFILES_HOME -path '*/bin'` ; do
   add_to_path $bin_dir
